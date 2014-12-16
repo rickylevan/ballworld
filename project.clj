e84ad72a851ef23f60fa0ba0d004af350c3f0a56
Ricky LeVan <rick.levan@gmail.com>
2014-12-15 22:22:54 -0600

    works sanely right now with two balls moving not too fast

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject ballworld "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.4"]
+                 [swing-utils "0.2.0"]
+                 [net.mikera/core.matrix "0.31.1"]
+                 [incanter "1.5.5"]]
+  :main ballworld.core)
